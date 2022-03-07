f86cf499c9cb75bd88b5cce1604284e410350cb3
becky <xieb3cky@gmail.com>
2022-03-06 23:44:07 -0500

    flask jinja

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def ask_questions():
+    """show questions form"""
+
+    prompts = story.prompts
+
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """show the story"""
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
